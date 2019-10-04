926622752125faaabaf0ba5c1f7234232010856a
Younes JAAIDI <yjaaidi@gmail.com>
2019-10-04 15:44:04 +0200

    🚧 add cache plugin specs. Co-authored-by: Koala <bozonedouard@gmail.com>

---
+++ libs/httpext/src/lib/cache-plugin.spec.ts
@@ -0,0 +1,13 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+describe('CachePlugin', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+  });
+
+  it.todo('🚧 should retrieve resource with exact same url once');
+  
+});
