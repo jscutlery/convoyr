a4f29ebfd6b87f6213a7656037488ad10371bc37
Younes JAAIDI <yjaaidi@gmail.com>
2019-10-04 17:30:55 +0200

    🚧 add cache plugin
    Co-authored-by: Koala <bozonedouard@gmail.com>

--- libs/httpext/src/lib/cache-plugin.ts
+++ libs/httpext/src/lib/cache-plugin.ts
@@ -1,15 +1,47 @@
 import { Observable, SubscribableOrPromise } from 'rxjs';
 
-type Request = unknown;
-type Response = unknown;
+export interface Headers {
+  [key: string]: string;
+}
 
-export type NextFn = ({req: Request, res: Response}) => Observable<Response>;
- 
+export type HttpMethod =
+  | 'HEAD'
+  | 'OPTIONS'
+  | 'GET'
+  | 'POST'
+  | 'PUT'
+  | 'PATCH'
+  | 'DELETE';
+
+export interface Request<T = unknown> {
+  readonly url: string;
+  readonly method: HttpMethod;
+  readonly body: T | null;
+  readonly headers: Headers;
+  readonly params: { [key: string]: string | string[] };
+}
+
+export interface Response<T = unknown> {
+  data: T;
+  status: number;
+  statusText: string;
+  headers: Headers;
+}
+
+export type NextFn = ({ req: Request, res: Response }) => Observable<Response>;
+
 export type SyncOrAsync<T> = T | SubscribableOrPromise<T>;
 
 export interface Plugin {
   condition({ req }: { req: Request }): boolean;
-  handle({ req, res }: { req: Request; res: Response; next: NextFn }): SyncOrAsync<Response>;
+  handle({
+    req,
+    res
+  }: {
+    req: Request;
+    res: Response;
+    next: NextFn;
+  }): SyncOrAsync<Response>;
 }
 
 export function useCachePlugin(): Plugin {
