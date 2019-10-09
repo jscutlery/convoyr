1a7b310f77ad5fdd23430b7603f9e09725db2af6
Koala <bozonedouard@gmail.com>
2019-10-09 12:26:40 +0200

    :construction: add conditional handling spec

--- libs/httpext/src/lib/http-ext.spec.ts
+++ libs/httpext/src/lib/http-ext.spec.ts
@@ -1,29 +1,29 @@
-import { of } from 'rxjs';
+import { of, Observable } from 'rxjs';
 
 import { Request, Response } from './http';
 import { HttpExt } from './http-ext';
 
 describe('HttpExt', () => {
-  function createSpyPlugin() {
+  let handler: (request: Request<any>) => Observable<Response<any>>;
+
+  function createSpyPlugin(
+    condition: (request: Request) => boolean = (request: Request) => true
+  ) {
     return {
+      condition: jest.fn(({ request }) => condition(request)),
       handle: jest.fn(({ request, next }) => next({ request }))
     };
   }
 
-  const httpResponse: Response = {
-    data: { answer: 42 },
-    status: 200,
-    statusText: 'ok',
-    headers: {}
-  };
-
-  const httpRequest: Request = {
-    url: 'https://answer-to-the-ultimate-question-of-life.com',
-    method: 'GET',
-    body: null,
-    headers: {},
-    params: {}
-  };
+  beforeEach(() => {
+    handler = request =>
+      of({
+        data: { answer: 42 },
+        status: 200,
+        statusText: 'ok',
+        headers: {}
+      });
+  });
 
   it('should handle multiple plugins', done => {
     const pluginA = createSpyPlugin();
@@ -31,9 +31,15 @@ describe('HttpExt', () => {
     const pluginB = createSpyPlugin();
     const mockHandleB = pluginB.handle;
     const httpExt = new HttpExt({ plugins: [pluginA, pluginB] });
-    const handler = (req: Request) => of(httpResponse);
+    const request: Request = {
+      url: 'https://answer-to-the-ultimate-question-of-life.com',
+      method: 'GET',
+      body: null,
+      headers: {},
+      params: {}
+    };
 
-    const response = httpExt.handle({ request: httpRequest, handler });
+    const response = httpExt.handle({ request, handler });
 
     expect(mockHandleA.mock.calls.length).toBe(1);
     expect(typeof mockHandleA.mock.calls[0][0].next).toBe('function');
@@ -44,7 +50,7 @@ describe('HttpExt', () => {
       headers: {},
       params: {}
     });
-    expect(mockHandleA.mock.calls.length).toBe(1);
+    expect(mockHandleB.mock.calls.length).toBe(1);
     expect(typeof mockHandleB.mock.calls[0][0].next).toBe('function');
     expect(mockHandleB.mock.calls[0][0].request).toEqual({
       url: 'https://answer-to-the-ultimate-question-of-life.com',
@@ -53,6 +59,62 @@ describe('HttpExt', () => {
       headers: {},
       params: {}
     });
+
+    response.subscribe(resp => {
+      expect(resp).toEqual({
+        data: { answer: 42 },
+        status: 200,
+        statusText: 'ok',
+        headers: {}
+      });
+      done();
+    });
+  });
+
+  it('should conditionally handle plugins', done => {
+    const pluginA = createSpyPlugin(req =>
+      req.url.includes('the-ultimate-question-of-life')
+    );
+    const pluginB = createSpyPlugin(req =>
+      req.url.includes('something-else-that-do-not-match')
+    );
+    const httpExt = new HttpExt({ plugins: [pluginA, pluginB] });
+    const request: Request = {
+      url: 'https://answer-to-the-ultimate-question-of-life.com',
+      method: 'GET',
+      body: null,
+      headers: {},
+      params: {}
+    };
+
+    const response = httpExt.handle({ request, handler });
+
+    expect(pluginA.condition.mock.calls[0][0].request).toEqual({
+      url: 'https://answer-to-the-ultimate-question-of-life.com',
+      method: 'GET',
+      body: null,
+      headers: {},
+      params: {}
+    });
+    expect(pluginA.condition.mock.results[0].value).toBeTruthy();
+    expect(pluginA.handle.mock.calls.length).toBe(1);
+    expect(pluginA.handle.mock.calls[0][0].request).toEqual({
+      url: 'https://answer-to-the-ultimate-question-of-life.com',
+      method: 'GET',
+      body: null,
+      headers: {},
+      params: {}
+    });
+
+    expect(pluginB.condition.mock.calls[0][0].request).toEqual({
+      url: 'https://answer-to-the-ultimate-question-of-life.com',
+      method: 'GET',
+      body: null,
+      headers: {},
+      params: {}
+    });
+    expect(pluginB.condition.mock.results[0].value).toBeFalsy();
+    expect(pluginB.handle.mock.calls.length).toBe(0);
 
     response.subscribe(resp => {
       expect(resp).toEqual({
