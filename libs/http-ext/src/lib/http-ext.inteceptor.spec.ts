e78d7a22ecdb7d8281f25592ba3c10a55780e892
Koala <bozonedouard@gmail.com>
2019-10-10 17:00:11 +0200

    :hammer_and_wrench: add  interceptor spec

--- libs/http-ext/src/lib/http-ext.inteceptor.spec.ts
+++ libs/http-ext/src/lib/http-ext.inteceptor.spec.ts
@@ -4,7 +4,7 @@ import { createRequest } from './request';
 import { HttpRequest } from '@angular/common/http';
 
 describe('HttpExtInterceptor', () => {
-  xit('🚧 should call handler', () => {
+  it('should call handler', () => {
     const httpExt = new HttpExt({ plugins: [] });
     const interceptor = new HttpExtInterceptor({ httpExt });
     const ngRequest = new HttpRequest('GET', 'https://test.com');
@@ -15,18 +15,32 @@ describe('HttpExtInterceptor', () => {
     /* Making a passthrough mock `HttpExt`. */
     jest
       .spyOn(httpExt, 'handle')
-      .mockImplementation(({request, handler}) => handler({ request }));
+      .mockImplementation(({ request, handler }) => handler({ request }));
 
     /* Go! */
     interceptor.intercept(ngRequest, next);
 
     /* Check that request is transformed from Angular HttpRequest to HttpExtRequest and forwarded to `httpExt`. */
-    expect(httpExt.handle).toHaveBeenCalledWith(expect.objectContaining({
-      request: createRequest({ url: 'https://test.com', method: 'GET'}),
-    }));
+    expect(httpExt.handle).toHaveBeenCalledTimes(1);
+    expect(httpExt.handle).toHaveBeenCalledWith(
+      expect.objectContaining({
+        request: createRequest({ url: 'https://test.com', method: 'GET' })
+      })
+    );
 
-    /* Check that request is transformed from HttpExtRequest to Angular HttpRequest. */
-    expect(next.handle).toHaveBeenCalledWith(new HttpRequest('GET', 'https://test.com'));
+    /* Check that request is transformed from HttpExtRequest to Angular HttpRequest when forwarded to Angular. */
+    expect(next.handle).toHaveBeenCalledTimes(1);
+
+    const forwardedNgRequest = next.handle.mock.calls[0][0];
+    forwardedNgRequest.headers.get('test');
+
+    expect(forwardedNgRequest).toBeInstanceOf(HttpRequest);
+    expect(forwardedNgRequest).toEqual(
+      expect.objectContaining({
+        method: 'GET',
+        url: 'https://test.com'
+      })
+    );
   });
 
   it.todo('🚧 should ignore HttpEvents except HttpResponse');
