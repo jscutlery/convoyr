b409d44de909ad91b4c92c1faeac41c36969f950
Koala <bozonedouard@gmail.com>
2019-10-11 10:37:06 +0200

    :hammer_and_wrench: remove todos

--- libs/http-ext/src/lib/http-ext.interceptor.ts
+++ libs/http-ext/src/lib/http-ext.interceptor.ts
@@ -1,12 +1,6 @@
-import {
-  HttpEvent,
-  HttpHandler,
-  HttpInterceptor,
-  HttpRequest,
-  HttpResponse
-} from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { filter, map, tap } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
 
 import { fromNgReq, fromNgResponse, toNgReq, toNgResponse } from './http-converter';
 import { HttpExt } from './http-ext';
@@ -22,11 +16,6 @@ export class HttpExtInterceptor implements HttpInterceptor {
     ngReq: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    /**
-     * @todo filter `HttpEvent`s which are not `HttpResponse`.
-     * @todo convert `HttpResponse` to http ext response after next.handle with a pipe map.
-     * @todo convert http ext response to `HttpResponse` after HttpExt.handle with a pipe map.
-     */
     return this._httpExt.handle({
       request: fromNgReq(ngReq),
       handler: ({ request }) =>
