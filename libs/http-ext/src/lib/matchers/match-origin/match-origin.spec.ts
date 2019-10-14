c7db2ab105608bbc061063dda746546ecc85aad9
Younes JAAIDI <yjaaidi@gmail.com>
2019-10-14 17:00:39 +0200

    🚧 add couple of test ideas to make sure that match origin matches origin and not url

--- libs/http-ext/src/lib/matchers/match-origin/match-origin.spec.ts
+++ libs/http-ext/src/lib/matchers/match-origin/match-origin.spec.ts
@@ -4,10 +4,13 @@ import { matchOrigin } from './match-origin';
 describe.each([
   /* Using a string */
   ['https://test.com', 'https://test.com', true],
+  // @todo ['https://test.com/test', 'https://test.com', true],
   ['https://test.com', 'https://angular.io', false],
+  // @todo ['https://test.com.test.com', 'https://test.com', false],
 
   /* Using an Array */
   ['https://test.com', ['https://test.com'], true],
+  // @todo ['https://test.com/test', ['https://test.com'], true],
   ['https://test.com', ['https://angular.io'], false],
 
   /* Using a RegExp */
