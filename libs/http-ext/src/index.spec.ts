9d07302c7508daec9ff200f5783f3894dcd347d7
Koala <bozonedouard@gmail.com>
2019-10-14 14:06:42 +0200

    :hammer_and_wrench: expose matchOrigin

--- libs/http-ext/src/index.spec.ts
+++ libs/http-ext/src/index.spec.ts
@@ -3,5 +3,6 @@ import * as publicApi from './index';
 describe('Public API', () => {
   it('should expose HttpExt', () => {
     expect(publicApi.HttpExtModule).toBeDefined();
+    expect(publicApi.matchOrigin).toBeDefined();
   });
 });
--- libs/http-ext/src/index.ts
+++ libs/http-ext/src/index.ts
@@ -1,2 +1,3 @@
 export { HttpExtModule } from './lib/http-ext.module';
 export { Plugin } from './lib/plugin';
+export { matchOrigin } from './lib/matchers/match-origin';
