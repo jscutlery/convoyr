89ad2a66fa8ff47e794cf5bf3141e5ae9addfae4
Younes JAAIDI <yjaaidi@gmail.com>
2019-10-16 09:50:01 +0200

    🚧 throw error if invalid url is given to `getOrigin`

--- libs/http-ext/src/lib/matchers/match-origin/get-origin.ts
+++ libs/http-ext/src/lib/matchers/match-origin/get-origin.ts
@@ -1,6 +1,9 @@
 export const getOrigin = (url: string): string => {
   const [scheme, urlWithoutScheme] = url.split('://');
 
-  const [, , host] = url.split(/\/|\?/);
-  return `${scheme}://${host}`;
+  const result = urlWithoutScheme.match(/^[a-z0-9.:-]+/);
+
+  const originWithoutScheme = result ? result[0] : null;
+
+  return `${scheme}://${originWithoutScheme}`;
 };
