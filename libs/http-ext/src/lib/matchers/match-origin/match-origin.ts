93e7f4ddf3871afffec6da9e7a6cfc041ad72631
Younes JAAIDI <yjaaidi@gmail.com>
2019-10-16 10:34:23 +0200

    🚧 fail if matchOrigin is given an valid match expression

--- libs/http-ext/src/lib/matchers/match-origin/match-origin.ts
+++ libs/http-ext/src/lib/matchers/match-origin/match-origin.ts
@@ -4,12 +4,26 @@ import { matchArrayOrigin } from './match-array-origin';
 import { OriginMatchExpression } from './origin-match-expression';
 import { matchPredicateOrigin } from './match-predicate-origin';
 import { matchRegExpOrigin } from './match-reg-exp-origin';
-import { matchStringOrigin } from './match-string-origin';
+import { matchStringOrigin, originStringMatcher } from './match-string-origin';
+import { OriginMatcher } from './origin-matcher';
+
+export const originMatcherList: OriginMatcher[] = [originStringMatcher];
 
 export const matchOrigin = (
   matchExpression: OriginMatchExpression
 ): RequestCondition => ({ request }): boolean => {
   const origin = getOrigin(request.url);
+
+  const matcher = originMatcherList.find(_matcher =>
+    _matcher.canHandle(matchExpression)
+  );
+
+  if (matcher != null) {
+    return matcher.handle({
+      matchExpression,
+      origin
+    });
+  }
 
   return [
     matchStringOrigin,
--- libs/http-ext/src/lib/matchers/match-origin/match-string-origin.ts
+++ libs/http-ext/src/lib/matchers/match-origin/match-string-origin.ts
@@ -1,16 +1,23 @@
 import { isString } from '../../utils/is-string';
 import { OriginMatchExpression } from './origin-match-expression';
+import { OriginMatcher } from './origin-matcher';
 
 export const matchStringOrigin = (
   origin: string,
   matcher: OriginMatchExpression
 ): boolean => isString(matcher) && matcher === origin;
 
-export const originStringMatcher = {
-  canHandle(expression: OriginMatchExpression) {
-    return isString(expression);
+export const originStringMatcher: OriginMatcher = {
+  canHandle(matchExpression: OriginMatchExpression) {
+    return isString(matchExpression);
   },
-  handle({ origin, expression }: { expression: string; origin: string }) {
-    return origin === expression;
+  handle({
+    origin,
+    matchExpression
+  }: {
+    matchExpression: string;
+    origin: string;
+  }) {
+    return origin === matchExpression;
   }
 };
