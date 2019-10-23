68c60885c9ec4afc7903ee25b58b9418721bf736
Koala <bozonedouard@gmail.com>
2019-10-23 16:54:06 +0200

    feat: ✅ add method matcher
    Add public exports.

--- libs/http-ext/src/index.ts
+++ libs/http-ext/src/index.ts
@@ -1,3 +1,4 @@
 export { HttpExtModule } from './lib/http-ext.module';
 export { HttpExtPlugin } from './lib/plugin';
 export { matchOrigin } from './lib/matchers';
+export { matchMethod } from './lib/matchers';
--- libs/http-ext/src/lib/matchers/index.ts
+++ libs/http-ext/src/lib/matchers/index.ts
@@ -1,1 +1,2 @@
 export { matchOrigin } from './match-origin/match-origin';
+export { matchMethod } from './match-method/match-method';
