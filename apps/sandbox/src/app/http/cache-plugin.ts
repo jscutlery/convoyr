0adc0f5138d76b326e253e2fedc55e9de692ea8c
Younes JAAIDI <yjaaidi@gmail.com>
2019-10-29 08:12:34 +0100

    🧪 cache plugin playground

--- apps/sandbox/src/app/http/cache-plugin.ts
+++ apps/sandbox/src/app/http/cache-plugin.ts
@@ -2,8 +2,6 @@ import { HttpExtPlugin } from '@http-ext/http-ext';
 import { defer, EMPTY, iif, merge, Observable, of } from 'rxjs';
 import { delay, map, shareReplay, takeUntil, tap } from 'rxjs/operators';
 
-const cache = {};
-
 // export function createCacheOptions({
 //   withCacheInfo
 // }: {
@@ -21,6 +19,25 @@ export interface CacheResponse<TData> {
   data: TData;
 }
 
+export const cache = {
+  get(url) {
+    const cacheData = this._load();
+    return cacheData[url];
+  },
+  set(url, response) {
+    const cacheData = this._load();
+    cacheData[url] = response;
+    this._save(cacheData);
+  },
+  _load() {
+    const raw = localStorage.getItem('http-ext-cache');
+    return raw ? JSON.parse(raw) : {};
+  },
+  _save(cacheData) {
+    return localStorage.setItem('http-ext-cache', JSON.stringify(cacheData));
+  }
+};
+
 export function cachePlugin({
   withCacheInfo = false
 }: { withCacheInfo?: boolean } = {}): HttpExtPlugin {
@@ -28,7 +45,7 @@ export function cachePlugin({
     handle({ request, next }) {
       const real$ = next({ request }).pipe(
         delay(1000),
-        tap(response => (cache[request.url] = response)),
+        tap(response => cache.set(request.url, response)),
         shareReplay({
           refCount: true,
           bufferSize: 1
@@ -36,7 +53,7 @@ export function cachePlugin({
       );
 
       const fromCache$ = defer(() => {
-        const response = cache[request.url];
+        const response = cache.get(request.url);
         return response ? of(response) : EMPTY;
       }).pipe(takeUntil(real$));
 
