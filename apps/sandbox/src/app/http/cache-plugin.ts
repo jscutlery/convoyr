1dfe97fa2fe9385bab5546e151803720461a201a
Koala <bozonedouard@gmail.com>
2019-10-30 13:57:53 +0100

    🧪 cache plugin playground
    Add cache strategy

--- apps/sandbox/src/app/http/cache-plugin.ts
+++ apps/sandbox/src/app/http/cache-plugin.ts
@@ -1,18 +1,79 @@
 import { HttpExtPlugin } from '@http-ext/http-ext';
-import { defer, EMPTY, iif, merge, Observable, of } from 'rxjs';
+import { defer, EMPTY, merge, Observable, of } from 'rxjs';
 import { delay, map, shareReplay, takeUntil, tap } from 'rxjs/operators';
 
-// export function createCacheOptions({
-//   withCacheInfo
-// }: {
-//   withCacheInfo: boolean;
-// }): {} {
-//   return {
-//     θhttpExt: {
-//       withCacheInfo
-//     }
-//   };
-// }
+// tslint:disable-next-line: nx-enforce-module-boundaries
+import { HttpExtResponse } from 'libs/http-ext/src/lib/response';
+// tslint:disable-next-line: nx-enforce-module-boundaries
+import { HttpExtRequest } from 'libs/http-ext/src/lib/request';
+// tslint:disable-next-line: nx-enforce-module-boundaries
+import { RequestHandlerFn } from 'libs/http-ext/src/lib/http-ext';
+
+export type CacheStrategyType = 'cacheThenNetwork';
+
+export interface CacheStrategy {
+  handle({
+    request,
+    next
+  }: {
+    request: HttpExtRequest<unknown>;
+    next: RequestHandlerFn;
+  }): Observable<HttpExtResponse<unknown>>;
+}
+
+export function invalidStrategyError(strategy: string) {
+  const strategies: CacheStrategyType[] = ['cacheThenNetwork'];
+  return new Error(
+    `InvalidStrategyError: could not handle ${strategy}, valid values are ${strategies.toString()}`
+  );
+}
+
+export function findStrategyOrThrow({
+  strategy,
+  withCacheInfo
+}: {
+  withCacheInfo: boolean;
+  strategy: CacheStrategyType;
+}): CacheStrategy {
+  switch (strategy) {
+    case 'cacheThenNetwork':
+      return new CacheThenNetworkStrategy({ withCacheInfo });
+
+    default:
+      throw invalidStrategyError(strategy);
+  }
+}
+
+export class CacheThenNetworkStrategy implements CacheStrategy {
+  constructor(private options: { withCacheInfo: boolean }) {}
+
+  handle({ request, next }) {
+    const real$ = next({ request }).pipe(
+      delay(1000),
+      tap(response => cache.set(request.url, response)),
+      shareReplay({
+        refCount: true,
+        bufferSize: 1
+      })
+    );
+
+    const fromCache$ = defer(() => {
+      const response = cache.get(request.url);
+      return response ? of(response) : EMPTY;
+    }).pipe(takeUntil(real$));
+
+    const { withCacheInfo } = this.options;
+
+    /* Order is important here because if we subscribe to fromCache$ first, it will subscribe to real$
+     * and `takeUntil` will immediately unsubscribe from it because the result is synchronous.
+     * If real$ is first, it will subscribe and the subscription will be shared with the `takeUntil`
+     * thanks to shareReplay. */
+    return merge(
+      _addCacheInfo({ source$: real$, withCacheInfo, isFromCache: false }),
+      _addCacheInfo({ source$: fromCache$, withCacheInfo, isFromCache: true })
+    );
+  }
+}
 
 export interface CacheResponse<TData> {
   isFromCache: boolean;
@@ -38,33 +99,24 @@ export const cache = {
   }
 };
 
-export function cachePlugin({
-  withCacheInfo = false
-}: { withCacheInfo?: boolean } = {}): HttpExtPlugin {
+export interface CachePluginOptions {
+  withCacheInfo: boolean;
+  strategy: CacheStrategyType;
+}
+
+export function cachePlugin(
+  {
+    strategy = 'cacheThenNetwork',
+    withCacheInfo = false
+  }: Partial<CachePluginOptions> = {
+    strategy: 'cacheThenNetwork',
+    withCacheInfo: false
+  }
+): HttpExtPlugin {
   return {
     handle({ request, next }) {
-      const real$ = next({ request }).pipe(
-        delay(1000),
-        tap(response => cache.set(request.url, response)),
-        shareReplay({
-          refCount: true,
-          bufferSize: 1
-        })
-      );
-
-      const fromCache$ = defer(() => {
-        const response = cache.get(request.url);
-        return response ? of(response) : EMPTY;
-      }).pipe(takeUntil(real$));
-
-      /* Order is important here because if we subscribe to fromCache$ first, it will subscribe to real$
-       * and `takeUntil` will immediately unsubscribe from it because the result is synchronous.
-       * If real$ is first, it will subscribe and the subscription will be shared with the `takeUntil`
-       * thanks to shareReplay. */
-      return merge(
-        _addCacheInfo({ source$: real$, withCacheInfo, isFromCache: false }),
-        _addCacheInfo({ source$: fromCache$, withCacheInfo, isFromCache: true })
-      );
+      const cacheStrategy = findStrategyOrThrow({ strategy, withCacheInfo });
+      return cacheStrategy.handle({ request, next });
     }
   };
 }
