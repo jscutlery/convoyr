41913a21cfb78a753ba6a4c1e041be537d47fe78
Koala <bozonedouard@gmail.com>
2019-11-03 17:52:11 +0100

    :construction: add cache plugin
    Implement missing specs.

--- libs/plugin-cache/src/lib/plugin-cache.spec.ts
+++ libs/plugin-cache/src/lib/plugin-cache.spec.ts
@@ -1,23 +1,31 @@
 import {
   createRequest,
   createResponse,
-  HttpExtPlugin
+  HttpExtRequest,
+  HttpExtResponse
 } from '@http-ext/http-ext';
 import { concat, of } from 'rxjs';
 import { marbles } from 'rxjs-marbles/jest';
 
 import { _addMetadata } from './add-cache-metadata';
 import { cachePlugin as createCachePlugin } from './plugin-cache';
+import { MemoryCacheProvider } from './providers/memory-provider';
 
 const objectContaining = jasmine.objectContaining;
 
 describe('CachePlugin', () => {
+  let request: HttpExtRequest;
+  let response: HttpExtResponse;
+
+  beforeEach(() => {
+    request = createRequest({ url: 'https://ultimate-answer.com' });
+    response = createResponse({ body: { answer: 42 } });
+  });
+
   it(
     'should serve cache when hydrated',
     marbles(m => {
       const cachePlugin = createCachePlugin({ addCacheMetadata: true });
-      const request = createRequest({ url: 'https://ultimate-answer.com' });
-      const response = createResponse({ body: { answer: 42 } });
       const networkResponse = _addMetadata({ isFromCache: false })(response);
       const cacheResponse = _addMetadata({ isFromCache: true })(response);
 
@@ -41,20 +49,46 @@ describe('CachePlugin', () => {
 
   it('should not apply metadata by default', () => {
     const cachePlugin = createCachePlugin();
-    const request = createRequest({ url: 'https://ultimate-answer.com' });
-    const response = createResponse({ body: { answer: 42 } });
     const next = () => of(response);
 
     const cacheResponse = cachePlugin.handle({ request, next }) as any;
     const spyObserver = jest.fn();
 
     cacheResponse.subscribe(spyObserver);
+
+    expect(spyObserver).toBeCalledTimes(1);
     expect(spyObserver).toBeCalledWith(
       objectContaining({ body: { answer: 42 } })
     );
   });
 
-  it.todo('should use `MemoryCacheProvider` by default');
-  it.todo('should store the cache using given provider');
+  it('should use `MemoryCacheProvider` by default', () => {
+    const cachePlugin = createCachePlugin();
+
+    expect((cachePlugin as any)._cacheProvider).toBeDefined();
+    expect((cachePlugin as any)._cacheProvider).toBeInstanceOf(
+      MemoryCacheProvider
+    );
+  });
+
+  it('should store the cache using given provider', () => {
+    const spyProvider = { set: jest.fn() };
+    const cachePlugin = createCachePlugin({
+      cacheProvider: spyProvider as any
+    });
+    const next = () => of(response);
+
+    const handler = cachePlugin.handle({ request, next }) as any;
+    handler.subscribe();
+
+    expect(spyProvider.set).toBeCalledTimes(1);
+    expect(spyProvider.set.mock.calls[0][0]).toBe(
+      'https://ultimate-answer.com'
+    );
+    expect(spyProvider.set.mock.calls[0][1]).toEqual(
+      objectContaining({ body: { answer: 42 } })
+    );
+  });
+
   it.todo('should allow retry with exponential time span');
 });
