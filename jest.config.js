fa9e72f7adfe06ba5327e8085a4ac87b7e26618a
Koala <bozonedouard@gmail.com>
2019-11-24 12:21:06 +0100

    :hammer_and_wrench: migrate `jest-preset-angular` to v8
    There is one remaining issue related to:
    https://github.com/thymikee/jest-preset-angular/issues/332
    https://github.com/just-jeb/angular-builders/issues/596

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,6 @@ module.exports = {
   },
   resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageReporters: ['html'],
+  coverageReporters: ['html', 'lcov'],
   passWithNoTests: true
 };
