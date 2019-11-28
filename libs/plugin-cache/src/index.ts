cd15b43c6e2d996f71ec00ac01915c6b7dab7ba3
Koala <bozonedouard@gmail.com>
2019-11-28 15:28:35 +0100

    :hammer_and_wrench: expose `StorageAdapter` interface

--- libs/plugin-cache/src/index.ts
+++ libs/plugin-cache/src/index.ts
@@ -1,2 +1,3 @@
 export { cachePlugin, CachePluginOptions } from './lib/plugin-cache';
 export { HttpExtCacheResponse } from './lib/metadata';
+export { StorageAdapter } from './lib/store-adapters/storage-adapter';
