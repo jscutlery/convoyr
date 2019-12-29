23268e3fedabdd2be375da891d639a94090f19fc
Koala <bozonedouard@gmail.com>
2019-12-29 18:21:29 +0100

    :hammer_and_wrench: improve  spec

--- libs/plugin-cache/src/lib/utils/parse-max-size.spec.ts
+++ libs/plugin-cache/src/lib/utils/parse-max-size.spec.ts
@@ -1,14 +1,18 @@
 import { parseMaxSize } from './parse-max-size';
 
-describe('parseMaxSize', () => {
-  it('should parse max size', () => {
-    expect(parseMaxSize('14b')).toBe(14);
-    expect(parseMaxSize('1024')).toBe(1024);
-    expect(parseMaxSize('13kB')).toBe(13312);
-    expect(parseMaxSize('1MB')).toBe(1048576);
-    expect(parseMaxSize('1GB')).toBe(1073741824);
+describe.each<[string, number]>([
+  ['14b', 14],
+  ['1024', 1024],
+  ['13kB', 13312],
+  ['1MB', 1048576],
+  ['1GB', 1073741824]
+])('parseMaxSize with format: %p => %p', (prettySize, sizeInBytes) => {
+  it('should parse size in Bytes', () => {
+    expect(parseMaxSize(prettySize)).toBe(sizeInBytes);
   });
+});
 
+describe('parseMaxSize', () => {
   it('should return null if null or undefined', () => {
     expect(parseMaxSize(null)).toEqual(null);
     expect(parseMaxSize(undefined)).toEqual(null);
