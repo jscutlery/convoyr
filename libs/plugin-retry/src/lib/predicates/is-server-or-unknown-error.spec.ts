1a969c7694e1ff7ef66070fc900a0e6a7c802147
Younes Jaaidi <yjaaidi@gmail.com>
2020-01-13 21:30:00 +0100

    🚧 add plugin-retry: retry on server or unknown error

--- libs/plugin-retry/src/lib/predicates/is-server-or-unknown-error.spec.ts
+++ libs/plugin-retry/src/lib/predicates/is-server-or-unknown-error.spec.ts
@@ -3,8 +3,14 @@ import { RetryPredicate } from '@http-ext/plugin-retry';
 import { isServerError } from './is-server-error';
 import { isUnknownError } from './is-unknown-error';
 
+export const or = <TArgs extends any[]>(
+  ...predicates: ((...args: TArgs) => boolean)[]
+) => (...args: TArgs) => {
+  return predicates.some(predicate => predicate(...args));
+};
+
 export const isServerOrUnknownError: RetryPredicate = response => {
-  return isServerError(response) || isUnknownError(response);
+  return or(isServerError, isUnknownError)(response);
 };
 
 describe.each<[HttpExtResponse, boolean]>([
