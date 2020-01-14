63ccef5bc9a02ea1e1752254a2c795eb1011478a
Younes Jaaidi <yjaaidi@gmail.com>
2020-01-14 14:03:00 +0100

    🚧 add plugin-auth: add specs

--- libs/plugin-auth/src/lib/create-auth-plugin.spec.ts
+++ libs/plugin-auth/src/lib/create-auth-plugin.spec.ts
@@ -1,25 +1,42 @@
-import { HttpExtPlugin } from '@http-ext/core';
+import { createRequest, HttpExtPlugin } from '@http-ext/core';
 import { from, of } from 'rxjs';
+import { marbles } from 'rxjs-marbles';
 
 function createAuthPlugin(args): HttpExtPlugin {
   throw new Error('🚧 work in progress!');
 }
 
 describe('AuthPlugin', () => {
-  it('🚧 should add bearer token to each request', () => {
-    const token$ = of('TOKEN');
+  xit(
+    '🚧 should add bearer token to each request',
+    marbles(m => {
+      const token$ = m.cold('-t|', { t: 'TOKEN' });
 
-    const authPlugin = createAuthPlugin({
-      token: token$
-    });
-    const { handler } = authPlugin;
+      const authPlugin = createAuthPlugin({
+        token: token$
+      });
+      const { handler } = authPlugin;
 
-    // @todo create a request
-    // @todo handle request
-    // @todo check request has header
-  });
+      const request = createRequest({
+        url: '/somewhere'
+      });
 
-  it('🚧 should grab the first token value only and run request once', () => {
+      const next = jest.fn();
+
+      handler.handle({
+        request,
+        next
+      });
+
+      expect(next).toHaveBeenCalledWith({
+        request: {
+          url: '/somewhere'
+        }
+      });
+    })
+  );
+
+  xit('🚧 should grab the first token value only and run request once', () => {
     const token$ = from(['TOKEN_1', 'TOKEN_2']);
 
     // @todo
