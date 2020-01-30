fdcd93814e302ed485672e0a0e4ae310eb488c4e
Younes Jaaidi <yjaaidi@gmail.com>
2020-01-31 00:57:03 +0100

    🚧 use dynamic configuration

--- libs/angular/src/lib/http-ext.module.spec.ts
+++ libs/angular/src/lib/http-ext.module.spec.ts
@@ -11,58 +11,98 @@ import { HttpExtModule } from './http-ext.module';
 describe('HttpExtModule', () => {
   let spyPlugin;
 
-  beforeEach(() => {
-    spyPlugin = createSpyPlugin();
+  describe('with config', () => {
+    beforeEach(() => {
+      spyPlugin = createSpyPlugin();
 
-    TestBed.configureTestingModule({
-      imports: [
-        HttpClientTestingModule,
-        HttpExtModule.forRoot({
-          plugins: [spyPlugin]
-        })
-      ]
+      TestBed.configureTestingModule({
+        imports: [
+          HttpClientTestingModule,
+          HttpExtModule.forRoot({
+            plugins: [spyPlugin]
+          })
+        ]
+      });
     });
-  });
 
-  let httpClient: HttpClient;
-  beforeEach(() => (httpClient = TestBed.inject(HttpClient)));
+    let httpClient: HttpClient;
+    beforeEach(() => (httpClient = TestBed.inject(HttpClient)));
 
-  let httpController: HttpTestingController;
-  beforeEach(() => (httpController = TestBed.inject(HttpTestingController)));
+    let httpController: HttpTestingController;
+    beforeEach(() => (httpController = TestBed.inject(HttpTestingController)));
 
-  afterEach(() => httpController.verify());
+    afterEach(() => httpController.verify());
 
-  it('should handle http request', () => {
-    const observer = jest.fn();
+    it('should handle http request', () => {
+      const observer = jest.fn();
 
-    httpClient
-      .get('https://jscutlery.github.io/items/ITEM_ID')
-      .subscribe(observer);
+      httpClient
+        .get('https://jscutlery.github.io/items/ITEM_ID')
+        .subscribe(observer);
 
-    httpController
-      .expectOne('https://jscutlery.github.io/items/ITEM_ID')
-      .flush({
+      httpController
+        .expectOne('https://jscutlery.github.io/items/ITEM_ID')
+        .flush({
+          id: 'ITEM_ID',
+          title: 'ITEM_TITLE'
+        });
+
+      expect(observer).toHaveBeenCalledTimes(1);
+      expect(observer.mock.calls[0][0]).toEqual({
         id: 'ITEM_ID',
         title: 'ITEM_TITLE'
       });
 
-    expect(observer).toHaveBeenCalledTimes(1);
-    expect(observer.mock.calls[0][0]).toEqual({
-      id: 'ITEM_ID',
-      title: 'ITEM_TITLE'
+      expect(spyPlugin.handler.handle).toHaveBeenCalledTimes(1);
+      expect(spyPlugin.handler.handle.mock.calls[0][0].request).toEqual({
+        url: 'https://jscutlery.github.io/items/ITEM_ID',
+        method: 'GET',
+        body: null,
+        headers: {},
+        params: {},
+        responseType: 'json'
+      });
+      expect(typeof spyPlugin.handler.handle.mock.calls[0][0].next).toEqual(
+        'function'
+      );
+    });
+  });
+
+  describe('with dynamic config', () => {
+    beforeEach(() => {
+      spyPlugin = createSpyPlugin();
+
+      TestBed.configureTestingModule({
+        imports: [
+          HttpClientTestingModule,
+          HttpExtModule.forRoot({
+            plugins: [spyPlugin]
+          })
+        ]
+      });
     });
 
-    expect(spyPlugin.handler.handle).toHaveBeenCalledTimes(1);
-    expect(spyPlugin.handler.handle.mock.calls[0][0].request).toEqual({
-      url: 'https://jscutlery.github.io/items/ITEM_ID',
-      method: 'GET',
-      body: null,
-      headers: {},
-      params: {},
-      responseType: 'json'
+    let httpClient: HttpClient;
+    beforeEach(() => (httpClient = TestBed.inject(HttpClient)));
+
+    let httpController: HttpTestingController;
+    beforeEach(() => (httpController = TestBed.inject(HttpTestingController)));
+
+    afterEach(() => httpController.verify());
+
+    xit('🚧 should handle http request', () => {
+      const observer = jest.fn();
+
+      httpClient
+        .get('https://jscutlery.github.io/items/ITEM_ID')
+        .subscribe(observer);
+
+      httpController
+        .expectOne('https://jscutlery.github.io/items/ITEM_ID')
+        .flush({});
+
+      expect(observer).toHaveBeenCalledTimes(1);
+      expect(spyPlugin.handler.handle).toHaveBeenCalledTimes(1);
     });
-    expect(typeof spyPlugin.handler.handle.mock.calls[0][0].next).toEqual(
-      'function'
-    );
   });
 });
