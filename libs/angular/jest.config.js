08bb7d4e60b8ec4d0ac4b05e4b3a53c1ec0b40ce
Koala <bozonedouard@gmail.com>
2020-03-29 16:27:04 +0200

    chore: 🛠 bump all major packages

--- libs/angular/jest.config.js
+++ libs/angular/jest.config.js
@@ -3,8 +3,9 @@ module.exports = {
   preset: '../../jest.config.js',
   coverageDirectory: '../../coverage/libs/angular',
   snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
   ],
   coverageReporters: ['html', 'lcov']
 };
