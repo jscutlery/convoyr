183a6f8f46eaf1b21eaef5ef482600accda04b5e
Koala <bozonedouard@gmail.com>
2020-04-02 10:44:54 +0200

    🚧 add search query

--- apps/sandbox-api/src/main.ts
+++ apps/sandbox-api/src/main.ts
@@ -8,14 +8,28 @@ app.use(cors());
 
 app.get('/api/bikes', (req, res) => {
   const rawBikes = readFileSync(__dirname + '/assets/bikes.json', 'utf8');
+  const { bikes } = JSON.parse(rawBikes);
 
-  res.json(JSON.parse(rawBikes));
+  if (req.query.q) {
+    return res.json({
+      bikes: bikes.filter(({ name }) =>
+        name.toLowerCase().includes(req.query.q.toLowerCase())
+      )
+    });
+  }
+
+  res.json({ bikes });
 });
 
 app.get('/api/bikes/:bikeId', (req, res) => {
   const rawBikes = readFileSync(__dirname + '/assets/bikes.json', 'utf8');
   const { bikes } = JSON.parse(rawBikes);
   const bike = bikes.find(_bike => _bike.id === req.params.bikeId);
+
+  if (bike == null) {
+    return res.sendStatus(404);
+  }
+
   res.json(bike);
 });
 
