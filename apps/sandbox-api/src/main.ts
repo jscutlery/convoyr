b36176329024d61237019ef6ad63b581681afdc4
Koala <bozonedouard@gmail.com>
2020-04-08 11:13:11 +0200

    🛠  sandbox auth: add users protected route

--- apps/sandbox-api/src/main.ts
+++ apps/sandbox-api/src/main.ts
@@ -7,6 +7,7 @@ import { getBike } from './app/get-bike';
 import { throwServerError } from './app/throw-server-error';
 import { throwUnauthorizedError } from './app/throw-unauthorized-error';
 import { createToken } from './app/token';
+import { getUsers } from './app/get-users';
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/api/bikes', getBikes);
+app.get('/api/users', getUsers);
 app.get('/api/bikes/:bikeId', getBike);
 app.get('/api/server-error', throwServerError);
 app.get('/api/unauthorized-error', throwUnauthorizedError);
