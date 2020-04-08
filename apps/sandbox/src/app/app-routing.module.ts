cf8acb2a0b6c1b0c76ccf079ae3a1ee1f0f96bf1
Younes Jaaidi <yjaaidi@gmail.com>
2020-04-08 12:20:30 +0200

    fix(sandbox): 🐞 fix routing

--- apps/sandbox/src/app/app-routing.module.ts
+++ apps/sandbox/src/app/app-routing.module.ts
@@ -5,21 +5,13 @@ import { IsSignedInGuard } from './auth/is-signed-in.guard';
 import { BikeDetailComponent } from './bike-detail/bike-detail.component';
 import { BikeSearchComponent } from './bike-search/bike-search.component';
 import { ErrorComponent } from './error/error.component';
-<<<<<<< HEAD
-import { SignInComponent } from './sign-in/sign-in.component';
-import { UsersComponent } from './users/users.component';
-=======
 import { SigninComponent } from './signin/signin.component';
->>>>>>> feat(sandbox): ✅ redirect to bikes after signin
+import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
   { path: 'signin', component: SigninComponent },
   { path: 'error', component: ErrorComponent },
-<<<<<<< HEAD
-  { path: '', redirectTo: 'bikes' },
-=======
   { path: '', pathMatch: 'full', redirectTo: 'bikes' },
->>>>>>> feat(sandbox): ✅ redirect to bikes after signin
   {
     path: '',
     canActivate: [IsSignedInGuard],
