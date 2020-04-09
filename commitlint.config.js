828c74e6dfda7db9340a9541de61b62c771a6642
Koala <bozonedouard@gmail.com>
2020-04-09 11:51:53 +0200

    chore: 🛠  overide commitlint type-enum

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['chore', 'docs', 'feat', 'fix']]
+  }
 };
