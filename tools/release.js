f34170bfaddb5c78a0b461a154e92edec39d8e45
Younes Jaaidi <yjaaidi@gmail.com>
2020-04-14 11:09:52 +0200

    chore: 🛠 make sure we don't miss a library on publish

--- tools/release.js
+++ tools/release.js
@@ -4,14 +4,6 @@ const exec = require('child_process').execSync;
 
 const execSync = (command) => exec(command, { stdio: 'inherit' });
 
-const libraries = [
-  'core',
-  'angular',
-  'plugin-auth',
-  'plugin-cache',
-  'plugin-retry',
-];
-
 /* Create new release */
 execSync('lerna version --no-push --conventional-commits --yes');
 
@@ -19,11 +11,7 @@ execSync('lerna version --no-push --conventional-commits --yes');
 execSync('nx affected:build --prod --all');
 
 /* Publish to NPM */
-for (const library of libraries) {
-  execSync(
-    'yarn publish --non-interactive --access public dist/libs/' + library
-  );
-}
+execSync('yarn workspaces run publish --non-interactive --access public');
 
 /* Update `latest` tag and push to GitHub */
 execSync('git tag -d latest && git tag latest && git push --follow-tags');
