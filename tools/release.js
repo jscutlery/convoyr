6923137ce369af6d1d47cdd9f67cf7cb228fa472
Koala <bozonedouard@gmail.com>
2020-04-16 09:46:19 +0200

    chore: 🛠 build libraries in band
    Build affetcted libs from latest tag in CI and all libs in release.

--- tools/release.js
+++ tools/release.js
@@ -8,7 +8,7 @@ const execSync = (command) => exec(command, { stdio: 'inherit' });
 execSync('lerna version --no-push --conventional-commits --yes');
 
 /* Build all libraries */
-execSync('yarn nx affected:build --with-deps --prod');
+execSync('yarn nx affected:build --all --with-deps --prod');
 
 /* Publish to NPM */
 execSync('yarn workspaces run publish --non-interactive --access public');
