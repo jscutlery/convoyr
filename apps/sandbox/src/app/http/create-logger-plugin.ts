5011110e103aaf5a92e7a95be14649484135a68f
Koala <bozonedouard@gmail.com>
2020-04-21 15:14:04 +0200

    chore(sandbox): 🚧 add operators
    Add sandbox example.

--- apps/sandbox/src/app/http/create-logger-plugin.ts
+++ apps/sandbox/src/app/http/create-logger-plugin.ts
@@ -1,12 +1,16 @@
-import { HttpExtPlugin } from '@http-ext/core';
+import { HttpExtPlugin, and, matchMethod, matchOrigin } from '@http-ext/core';
 
 export function createLoggerPlugin(): HttpExtPlugin {
   return {
+    shouldHandleRequest: and(
+      matchMethod('GET'),
+      matchOrigin('http://localhost:3333')
+    ),
     handler: {
       handle({ request, next }) {
         console.log(`[${request.method}] ${request.url}`);
         return next({ request });
-      }
-    }
+      },
+    },
   };
 }
