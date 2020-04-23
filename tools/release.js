77c5d091b3e8547a380f78eab183efab1c1d923c
Koala <bozonedouard@gmail.com>
2020-04-23 15:53:16 +0200

    chore: 🛠 update release script
    Use Lerna `publish` command for publishing packages.

--- tools/release.js
+++ tools/release.js
@@ -4,14 +4,11 @@ const exec = require('child_process').execSync;
 
 const execSync = (command) => exec(command, { stdio: 'inherit' });
 
-/* Create new release */
-execSync('lerna version --no-push --conventional-commits --yes');
-
 /* Build all libraries */
-execSync('yarn nx affected:build --all --with-deps --prod');
+execSync('nx affected:build --all --with-deps --prod');
 
 /* Publish to NPM */
-execSync('yarn workspaces run publish --access public --tag latest');
+execSync('lerna publish --yes');
 
 /* Push to GitHub */
 execSync('git push --follow-tags');
