04b4a589374531c9b34f5a6c215bb3dfd01a5bf1
Koala <bozonedouard@gmail.com>
2020-04-24 10:41:04 +0200

    chore: 🛠  improve build speed

--- tools/release.js
+++ tools/release.js
@@ -5,7 +5,9 @@ const exec = require('child_process').execSync;
 const execSync = (command) => exec(command, { stdio: 'inherit' });
 
 /* Build all libraries */
-execSync('nx affected:build --all --prod --parallel');
+execSync(
+  'nx affected:build --exclude sandbox --all --parallel --maxParallel 10 --prod'
+);
 
 /* Publish to NPM */
 execSync('lerna publish --dist-tag latest --yes');
