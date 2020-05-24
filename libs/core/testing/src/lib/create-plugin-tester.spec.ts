d0ae1aeb95b5418b7008d5e92ea51e3546cfd1c6
Koala <bozonedouard@gmail.com>
2020-05-24 17:35:32 +0200

    wip: 🚧 refactor tester plugin

--- libs/core/testing/src/lib/create-plugin-tester.spec.ts
+++ libs/core/testing/src/lib/create-plugin-tester.spec.ts
@@ -1,4 +1,19 @@
+import { PluginTester, createPluginTester } from './create-plugin-tester';
+
 describe('PluginTester', () => {
-  it.todo('should mock Http handler');
+  let pluginHandler: { handle: () => jest.Mock<any, any> };
+  let pluginTester: PluginTester;
+
+  beforeEach(() => (pluginHandler = { handle: jest.fn() }));
+  beforeEach(
+    () => (pluginTester = createPluginTester({ handler: pluginHandler as any }))
+  );
+
+  it('should mock Http handler', () => {
+    const httpHandlerMock = pluginTester.mockHttpHandler();
+
+    expect(jest.isMockFunction(httpHandlerMock)).toBeTruthy();
+  });
+
   it.todo('should run the Http handler using the mock');
 });
