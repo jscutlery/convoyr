eda8af0d95a4c7b30e7edafde8b5d7cf80bd9b2e
Koala <bozonedouard@gmail.com>
2020-06-13 12:33:26 +0200

    chore(sandbox): 🛠 fix plugin condition

--- apps/sandbox/src/app/http/reject-unknown-origins-plugin.ts
+++ apps/sandbox/src/app/http/reject-unknown-origins-plugin.ts
@@ -1,9 +1,8 @@
 import { ConvoyrPlugin, matchOrigin, not } from '@convoyr/core';
 import { throwError } from 'rxjs';
-import { environment } from '../../environments/environment';
 
 export const rejectUnknownOriginsPlugin: ConvoyrPlugin = {
-  shouldHandleRequest: not(matchOrigin('https://localhost:3333')),
+  shouldHandleRequest: not(matchOrigin('http://localhost:3333')),
   handler: {
     handle({ request }) {
       return throwError(`🛑 Requesting invalid origin, url: ${request.url}`);
